c26867e27920d9d1236f964f66df442a026c9e01
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-06 00:03:57 +0600

    slider added

---
+++ src/components/Slider.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../style.css";
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import slider1 from "../assets/slider1.jpg";
+import slider2 from "../assets/slider2.jpg";
+import slider3 from "../assets/slider3.jpg";
+
+export default function Slider() {
+  return (
+    <div className="mb-8">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div
+            className="hero min-h-[500px]"
+            style={{
+              backgroundImage: `url(${slider1})`,
+            }}
+          >
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-neutral-content text-center">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">
+                  Experience the Thrill of Football
+                </h1>
+                <p className="mb-5">
+                  Step onto the field where dreams come alive! Football ignites
+                  passion, strengthens bonds, and tests your skill with every
+                  pass, tackle, and goal. Whether you're playing or cheering,
+                  the game connects hearts across the globe.
+                </p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="hero min-h-[500px]"
+            style={{
+              backgroundImage: `url(${slider2})`,
+            }}
+          >
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-neutral-content text-center">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">
+                  Embrace the Elegance of Tennis
+                </h1>
+                <p className="mb-5">
+                  From the sound of the racket striking the ball to the thrill
+                  of match points, tennis is a blend of agility, strategy, and
+                  precision. Step into the court and let the game inspire your
+                  competitive spirit.
+                </p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="hero min-h-[500px]"
+            style={{
+              backgroundImage: `url(${slider3})`,
+            }}
+          >
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-neutral-content text-center">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">
+                  Feel the Spirit of Cricket
+                </h1>
+                <p className="mb-5">
+                  The gentleman's game, cricket, is more than just a sport—it's
+                  a tradition. With every boundary, every wicket, and every run,
+                  experience the camaraderie, strategy, and grace that make
+                  cricket timeless.
+                </p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Slider from '../components/Slider'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <header>
+        <Slider></Slider>
+      </header>
+    </div>
   )
 }
